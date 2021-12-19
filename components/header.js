c349e9072b9358776e2f532d268bf166f7e75184
CristianMoisei <cr.moisei@gmail.com>
2021-12-20 00:49:39 +0200

    Added a mobile nav menu

--- components/header.js
+++ components/header.js
@@ -1,3 +1,11 @@
+function openMenu() {
+    document.getElementById('nav').style.display = 'flex'
+}
+
+function closeMenu() {
+    document.getElementById('nav').style.display = 'none'
+}
+
 document.write(`
     <div id="header">
     <div id="header-container">
@@ -6,7 +14,9 @@ document.write(`
             <a href="">Newsletter</a>
             <a target="_blank" href="https://twitter.com/cristianmoisei">Twitter</a>
             <a target="_blank">Medium</a>
+            <a id="close-button" onClick="closeMenu()">Close</a>
         </div>    
+        <img id="menu" src="./assets/menu.svg" onClick="openMenu()">
     </div>
     </div>
 `);
