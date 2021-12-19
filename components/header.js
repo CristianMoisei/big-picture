bb61abe79045d212fc47400568fcdb6f6df6cf1c
CristianMoisei <cr.moisei@gmail.com>
2021-12-19 23:15:59 +0200

    Extracted header and meta as js components

---
+++ components/header.js
@@ -0,0 +1,13 @@
+document.write(`
+    <div id="header">
+    <div id="header-container">
+        <a href="index.html"><img id="logo" src="assets/logo.svg"></a>
+        <div id="nav">
+            <a href="">Newsletter</a>
+            <a href="about.html">About</a>
+            <a class="hidden-on-mobile" target="_blank" href="https://twitter.com/cristianmoisei">Twitter</a>
+            <a class="hidden-on-mobile" target="_blank">Medium</a>
+        </div>    
+    </div>
+    </div>
+`);
---
+++ components/meta.js
@@ -0,0 +1,10 @@
+document.write(`
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+    <meta name="theme-color" content="#02151F">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Let's cut through the noise with unbiased analyses of some of the biggest issues in the world">
+    <meta name="author" content="Cristian Moisei">
+`);
