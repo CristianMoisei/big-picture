8427a945389b7c2596c5160ba3ab008ea34bd12a
CristianMoisei <cr.moisei@gmail.com>
2022-03-02 00:13:19 +0100

    Added bLazy

--- scripts/renderChart.js
+++ scripts/renderChart.js
@@ -1,5 +1,11 @@
 function renderChart(id, hidden){
-    document.write(`<iframe id="datawrapper-chart-${id}" src="https://datawrapper.dwcdn.net/${id}" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="400"></iframe>`)
+
+    if (hidden) {
+        document.write(`<iframe id="datawrapper-chart-${id}" src="https://datawrapper.dwcdn.net/${id}" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="400"></iframe>`)
+    } else {
+        document.write(`<iframe class="b-lazy" id="datawrapper-chart-${id}" data-src="https://datawrapper.dwcdn.net/${id}" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="400"></iframe>`)
+    }
+
     !function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();
 
     if (hidden) { document.getElementById("datawrapper-chart-" + id).style.display = 'none' }    
